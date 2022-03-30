ad2b4f0f1c12e1d8e89605f4a84b63c188b6976a
Namrath <82800026+Namrath02@users.noreply.github.com>
2022-03-30 16:45:52 +0530

    Update materials.js

--- js/materials.js
+++ js/materials.js
@@ -1,7 +1,7 @@
 import * as THREE from "https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js";
 
 function vertexShader() {
-  return `varying vec3 vUv; 
+    return `varying vec3 vUv; 
       
                   void main() {
                     vUv = position; 
@@ -10,30 +10,29 @@ function vertexShader() {
                     gl_Position = projectionMatrix * modelViewPosition; 
                   }`;
 }
+
 function fragmentShader() {
-  return `uniform vec3 colorA; 
-                  uniform vec3 colorB; 
-                  varying vec3 vUv;
-    
-                  void main() {
-                gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
-                  }`;
+    return `uniform vec3 colorA; 
+                    uniform vec3 colorB; 
+                    varying vec3 vUv;
+      
+                    void main() {
+                  gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
+                    }`;
 }
 
-// Materials Function
+
 export function createMaterials() {
-  const cubeShader = new THREE.ShaderMaterial({
-    uniforms: {
-      colorA: { type: "vec3", value: new THREE.Color(0xff0000) },
-      colorB: { type: "vec3", value: new THREE.Color(0x0000ff) },
-    },
-    vertexShader: vertexShader(),
-    fragmentShader: fragmentShader(),
-  });
+    const cubeShader = new THREE.ShaderMaterial({
+        uniforms: {
+            colorA: { type: "vec3", value: new THREE.Color(0xff0000) },
+            colorB: { type: "vec3", value: new THREE.Color(0x0000ff) },
+        },
+        vertexShader: vertexShader(),
+        fragmentShader: fragmentShader(),
+    });
 
-  return {
-    cubeShader,
-  };
+    return {
+        cubeShader,
+    };
 }
-
-// Path: exp1/materials.js 
